238acf076bdc7c71b4451250e0bd74af986f1cbb
Akkahshh <akkahshh24@gmail.com>
2024-01-28 20:07:55 +0530

    add lazy loading and Suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./pages/home"
-import Search from "./pages/Search"
-import Cart from "./pages/Cart"
+import { lazy, Suspense } from 'react'
+import Loading from "./components/Loading"
+
+const Home = lazy(() => import("./pages/Home"))
+const Search = lazy(() => import("./pages/Search")) 
+const Cart = lazy(() => import("./pages/Cart"))
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/search" element={<Search></Search>}></Route>
-        <Route path="/cart" element={<Cart></Cart>}></Route>
+      <Suspense fallback={<Loading></Loading>}>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/search" element={<Search></Search>}></Route>
+          <Route path="/cart" element={<Cart></Cart>}></Route>
       </Routes>
+      </Suspense>
     </Router>
   )
 }
